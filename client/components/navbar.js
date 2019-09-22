28b2ae91972581c6d66a21cde77a71e213a5b133
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-09-22 17:09:41 -0500

    start on nav update

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,16 +3,94 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { logout } from '../store';
-import { Button, Header } from 'semantic-ui-react';
+import {
+  Button,
+  Header,
+  Menu,
+  Dropdown,
+  Container,
+  Image
+} from 'semantic-ui-react';
 
 const Navbar = ({ handleClick, isLoggedIn, isAdmin }) => (
   <div>
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item as={NavLink} to="/" header>
+          <Image size="mini" src="/logo.png" style={{ marginRight: '1.5em' }} />
+          Cody & Co. Pet Store
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/home">
+          Home
+        </Menu.Item>
+
+        <Dropdown item simple text="Adopt a Pet">
+          <Dropdown.Menu>
+            <Dropdown.Item as={NavLink} to="/products">
+              All Pets
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>
+              <i className="dropdown icon" />
+              <span className="text">By Species</span>
+              <Dropdown.Menu>
+                <Dropdown.Item as={NavLink} to="/products?categoryTag=1Dog">
+                  Dog
+                </Dropdown.Item>
+                <Dropdown.Item as={NavLink} to="/products?categoryTag=1Reptile">
+                  Reptile
+                </Dropdown.Item>
+                <Dropdown.Item as={NavLink} to="/products?categoryTag=1Cat">
+                  Cat
+                </Dropdown.Item>
+                <Dropdown.Item as={NavLink} to="/products?categoryTag=1Panda">
+                  Panda
+                </Dropdown.Item>
+                <Dropdown.Item as={NavLink} to="/products?categoryTag=1Hamster">
+                  Hamster
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Menu.Item as={NavLink} to="/cart">
+          Cart
+        </Menu.Item>
+        {isAdmin && (
+          <div>
+            <Menu.Item as={NavLink} to="/orders">
+              Company Orders
+            </Menu.Item>
+            <Menu.Item as={NavLink} to="/users">
+              All Users
+            </Menu.Item>
+            <Menu.Item as={NavLink} to="/productForm">
+              Create New Product
+            </Menu.Item>
+          </div>
+        )}
+        {isLoggedIn ? (
+          <Menu.Item onClick={handleClick}>Logout</Menu.Item>
+        ) : (
+          <div>
+            <Menu.Item as={NavLink} to="/login">
+              Login
+            </Menu.Item>
+            <Menu.Item as={NavLink} to="/signup">
+              Sign Up
+            </Menu.Item>
+          </div>
+        )}
+      </Container>
+    </Menu>
+
+    {/* </Menu>
     <NavLink to="/">
       <Header className="mainHeader" as="h1" size="huge" textAlign="center">
         {' '}
-        Cody and Co.{' '}
+        Cody & Co. Pet Store{' '}
       </Header>
-    </NavLink>
+    </NavLink> */}
     <nav>
       {isLoggedIn ? (
         <div>
